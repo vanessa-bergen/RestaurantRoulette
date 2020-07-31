afb3eaecfcac0ac15fe0eec6662b18ec2768eea6
Vanessa Bergen <vanessa.bergen1@gmail.com>
2020-07-31 08:43:43 -0700

    Initial Commit

---
+++ RestaurantRoulette/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestaurantRoulette
+//
+//  Created by Vanessa Bergen on 2020-07-31.
+//  Copyright © 2020 Vanessa Bergen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
